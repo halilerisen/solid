338926730778abda1b789e179859a2f55956cdce
h <H>
2022-11-17 17:04:07 +0300

    add Problem Interface Segregation Principle (ISP)

---
+++ ISP/problem/card_payment.dart
@@ -0,0 +1,21 @@
+import 'payment_service.dart';
+
+class CardPayment implements PaymentService {
+  @override
+  void cashBack() {}
+
+  @override
+  void installment() {}
+
+  @override
+  void pay() {}
+
+  @override
+  void payCoupon() {}
+
+  @override
+  void payCrypto() {}
+
+  @override
+  void payWallet() {}
+}
